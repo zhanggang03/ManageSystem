d29a9642d9b9306c6021496e3d7d2d5ff7b8aa7e
zhanggang <zhanggang2019@boco.com.cn>
2019-06-27 17:56:20 +0800

    增加User数据展示，已经Modal

--- src/app/dept/reducer.js
+++ src/app/dept/reducer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     ADD_USER,
     EDIT_USER,
@@ -6,20 +5,13 @@ import {
     SELECT_DEPT_FINISH,
     GET_ALLDEPT_FINISH,
 } from './action';
-=======
-import {ADD_USER, EDIT_USER, DELETE_USER, SELECT_DEPT_Finish} from './action';
->>>>>>> 95d6dec470939d38dbcf32dacc3a13458e86d170
 import {ADD_DEPT, EDIT_DEPT, DELETE_DEPT} from './action';
 
 let initialUserState = [];
 
 export const UserReducer = (state = initialUserState, action) => {
     switch (action.type) {
-<<<<<<< HEAD
         case SELECT_DEPT_FINISH:
-=======
-        case SELECT_DEPT_Finish:
->>>>>>> 95d6dec470939d38dbcf32dacc3a13458e86d170
             return action.payload;
         case ADD_USER:
             return state;
